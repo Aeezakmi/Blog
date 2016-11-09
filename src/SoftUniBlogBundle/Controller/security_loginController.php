d8972eadc0984a4b102bf58b49ae045728e375fd
Aeezakmi <extreem_aeezakmi_svilen@abv.bg>
2016-11-09 15:34:43 +0200

    Blog

---
+++ src/SoftUniBlogBundle/Controller/security_loginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SoftUniBlogBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class security_loginController extends Controller
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login()
+    {
+        return $this->render('security/login.html.twig');
+    }
+    /**
+     * This is the route the user can use to logout.
+     *
+     * But, this will never be executed. Symfony will intercept this first
+     * and handle the logout automatically. See logout in app/config/security.yml
+     *
+     * @Route("/logout", name="security_logout")
+     */
+    public function logoutAction()
+    {
+        throw new \Exception('This should never be reached!');
+    }
+}
